c10415df264bd2952fdfb1c814bc36ea5700de2e
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-10 14:52:26 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the lowercase alphabets
+ */
+void print_alphabet_x10(void)
+{	int j;
+	int x;
+
+	for (x = '0'; x < '10'; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
