c3cea897137cdc50b920eb5e2a2e9941997e9457
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-11 15:46:23 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio,h>
+
+/**
+ * main - prints the lowercase
+ * Description: prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
