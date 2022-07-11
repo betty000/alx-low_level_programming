f145119362b35d9e6e92f099b7f4728afeb6f31c
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-11 09:47:24 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,29 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			_putchar((i / 10)  + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10)  + '0');
-			_putchar((j % 10) + '0');
-			_putchar(('\n');
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10)  + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j > 10)
+			{
+				_putchar(':');
+				_putchar((j / 10)  + '0');
+				_putchar((j % 10) + '0');
+				_putchar(('\n');
+			}
 		}
 	}
 }
