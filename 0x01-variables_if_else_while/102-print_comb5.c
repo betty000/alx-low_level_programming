af5670ffc81dd75a0e36a63fd98daf16b4253f4c
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-12 12:39:19 -0700

    numers combination:

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,29 +6,23 @@
  */
 int main(void)
 {
-	int i, j, m;
+	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 100; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = 0; j <= 100; j++)
 		{
-			for ( m = 0; m <= 98; m++)
+			if (i < j)
 			{
-				if (i > j && m > i)
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar(j / 10 + 48);
-					putchar(j % 10 + 48);
+					putchar(',');
 					putchar(' ');
-					putchar(i / 10 + 48);
-					putchar(i % 10 + 48);
-					putchar(' ');
-					putchar(m / 10 + 48);
-					putchar(m % 10 + 48);
-					if (m != 98 || i != 99 || j != 100)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
 		}
