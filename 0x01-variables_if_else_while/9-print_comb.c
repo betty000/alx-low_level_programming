953512807c8ddb25004ae5c0b75085c9afb00c47
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-12 10:34:35 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,11 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		putchar((i % 0) + '0');
-		if (i != 9)
-		{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
 			putchar(',');
 			putchar('');
-		}
 	}
 	putchar('\n');
 	return (0);
