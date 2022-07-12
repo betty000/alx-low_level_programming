efa25db453c714ddd7669b404afe193065fd8530
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-12 09:44:42 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  numbers of base 16
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned char a = '0';
+
+	int 1;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
