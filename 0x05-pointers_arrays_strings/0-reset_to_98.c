d761e17ab4d701040fb019e9d5929ccbd24aa6bf
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-12 13:31:31 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - parameter and updates the value it points to to 9
+ * @n: pointer to be update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN.H
+#define MAIN.h
+int _putchar(char c );
+void reset_to_98(int *n);
+
+
+
+#endif
