b8a9b7ac9cfbc01fc5ef58a0faff3e868e770d2e
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-13 15:33:42 -0700

    prints the numbers from 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,18 @@ void fizz_buzz(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (x == 10)
+		if (x == 100)
 			printf("Buzz")
 		else if((x % 3 == 0) && (x % 5 == 0))
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (x % 3 == 0)
-			printf("Fizz");
-		else if (x % 5 ==0)
-			printf("Buzz")
-		else 
-			printf("%d", x);
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ")
+		else
+			printf("%d ", x);
 	}
-	printf('\n';}
+	printf('\n');
+}
