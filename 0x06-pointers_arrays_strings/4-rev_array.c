1af0982028b77391d49ea93bc4066f99ee1924d6
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-14 12:30:21 -0700

    for array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
  * reverse_array - reverses the content of an array of integers
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 	p = a;
 	for (i = 0; i < n; i++)
 		p++;
-	for (k = 0; k < i / 2; k++)
+	for (k = 0; k < i / 2; k--)
 	{
 		aux = a[k];
 		a[k] = *p;
