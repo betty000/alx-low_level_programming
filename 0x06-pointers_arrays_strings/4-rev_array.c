e053bb28bc2643e0b2c5047d0884a12183b02c4d
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-14 11:33:40 -0700

    content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array a
+ * @n: an element of an array
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
