5ad0b0d9798207d51da88331938cbbbebf7cd0bf
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-15 13:40:28 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ * Return: pointer s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char c1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char c2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; c1[j] != '\0'; j++)
+		{
+			if (c1[j] == s[i])
+			{
+				s[i] = c2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
