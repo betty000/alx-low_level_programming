7bf1f707b4f3de8741049dad6b13dbabe9d703a2
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-18 11:43:24 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with constant byte.
+ * @s: array of char
+ * @b: array of chat
+ * @n: number of bytes
+ * Return:(s)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n > 0)
+	{
+		*p = b;
+		p++;
+		n--;
+	}
+	return (s);
+}
