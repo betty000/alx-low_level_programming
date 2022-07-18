58398c22c38d534cf14444b863194a23091753d1
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-18 13:03:50 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ * Return: number of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, k, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (counter != i)
+			break;
+	for (k = 0; accept[k] != '\0'; k++)
+	{
+		if (s[i] == accept[k])
+			counter++;
+	}
+	return (counter);
+}
