6851a02f183d2d6cac47c85d9016cd005d49bf96
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-19 06:02:13 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array of pieces
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar('\n');
+	}
+}
