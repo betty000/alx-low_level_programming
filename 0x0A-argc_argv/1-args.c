50a22a6a99856087c54ca8134e89bfaa015a9c24
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-27 03:48:12 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,14 @@
 /**
  * main - print the num of args you passed to it
  * @args: argument count
- * @argv: argument vector, array of strings
+ * @argv: argument vector
  * Return: 0
  */
 int main(int args, char *argv[])
 {
-	printf("%d\n", args -1 );
 	(void)argv;
+
+	printf("%d\n", args -1 );
+
 	return (0);
 }
