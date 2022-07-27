e0ab9448ac045dcae3f8f142ddc4495e984a1026
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-27 02:50:42 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if char is lowercase
+ * @c: character to be checked if lower case, type int.
+ * Return: 1 if lower case, 0 otherwise.
+ */
+int _islower(int c)
+{
+
+	return (c >= 'a' && c <= 'z');
+}
