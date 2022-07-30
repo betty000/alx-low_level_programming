a59b8b6cb2af6b0502148dc64d1c988f5ffb256c
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-30 10:12:58 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked -  allocates memory
+ * @b: integer
+ * Return: 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
