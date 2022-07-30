9d53621605a6f24d1a9ba8185ca279eddf15fe94
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-07-30 11:01:51 -0700

    task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - Prints the program name.
+ * @argc: Number of commmand arguments
+ * @argv: Array of strings of the size of argc
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
