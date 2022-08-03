e3b2e2c74f63e536fabc2c6f308b86ee67ccf115
Bethelhem Ashenafi <bettyashu5@gmail.com>
2022-08-03 03:40:07 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints ints and hex
+ * @array:array of ints
+ * @size: size of array
+ * @action: a pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
